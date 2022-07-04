89ac364a8d42a0a4e8ff4146d902f0331e771249
jeum <eumjungmin1@gmail.com>
2022-07-04 15:01:27 +0900

    Add: db에 테이블 생성

--- owners/models.py
+++ owners/models.py
@@ -1,3 +1,19 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+class Owner(models.Model):
+    name = models.CharField(max_length=45)
+    email = models.CharField(max_length=300)
+    age = models.IntegerField
+
+    class Meta:
+        db_table = 'owners'
+
+class Dog(models.Model):
+    owner_id = models.ForeignKey('Owner', on_delete=models.CASCADE)
+    name = models.CharField(max_length=45)
+    age = models.IntegerField
+
+    class Meta:
+        db_table = 'dogs'
