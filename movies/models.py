be1470b8b68a869e2198cacb0023aca75d334150
jeum <eumjungmin1@gmail.com>
2022-07-05 14:56:40 +0900

    모델수정

--- movies/models.py
+++ movies/models.py
@@ -14,6 +14,7 @@ class Movie(models.Model):
     title = models.CharField(max_length=45)
     release_date = models.DateField()
     running_time = models.IntegerField()
+    
     class Meta:
         db_table = 'movies'
 
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+import json
 
+from django.http import JsonResponse
+from django.views import View
+
+from movies.models import *
 # Create your views here.
+
+class MoviesView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        
